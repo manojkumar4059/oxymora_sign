12c63fd3392b264f964833ac710f8391093fe2cd
Manoj Kumar <manoj@example.com>
2026-04-27 08:42:14 +0530

    Fixed byrth

--- app.py
+++ app.py
@@ -113,8 +113,8 @@ connect_mqtt()
 # ================= DATABASE MODELS (Code 1 Schema) =================
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    full_name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     otp = db.Column(db.String(6), nullable=True)
